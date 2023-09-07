fff3cd6c70fce3b1fccab25029484bfe5c4eb222
EheyGIGI <142112460+EheyGIGI@users.noreply.github.com>
2023-09-07 21:36:43 +0100

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -1,12 +1,29 @@
 
 
-// Adopting Dark styling when header scrolling
+// Adopting Dark styling when header scrolling down
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
+document.addEventListener('scroll', ()=> {
     if(window.scrollY > headerHeight){
         header.classList.add('header--dark');
     }else{
         header.classList.remove('header--dark');
+    }
+});
+
+//Transparenting home section when it's scrolling down
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', ()=> {
+   home.style.opacity = 1 - window.scrollY/homeHeight;
+});
+
+//visibling arrow icon when it's far away from home section
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=>{
+    if(window.scrollY > homeHeight / 2){
+        arrowUp.style.opacity = 1;
+    }else{
+        arrowUp.style.opacity = 0;
     }
 });
