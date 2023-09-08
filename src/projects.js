a21843a8566a88e8ea71f7897485a30326e56a31
EheyGIGI <142112460+EheyGIGI@users.noreply.github.com>
2023-09-08 21:14:35 +0100

    JS-Refactoring

--- src/projects.js
+++ src/projects.js
@@ -1,19 +1,42 @@
 'use strict';
 
-// Logic handling for Category filtering
 const categories = document.querySelector('.categories');
 const projects = document.querySelectorAll('.project');
+const projectContainer = document.querySelector('.projects');
 
+// Logic handling for filtering Categories
 categories.addEventListener('click', (event)=>{
     const filter = event.target.dataset.category;
+
     if(filter == null){return;}
     
+    handleActiveSelection(event.target);
+    filterProjects(filter);
+});
+
+//replacing active mode when it's clicked
+function handleActiveSelection(target){
+    const active = document.querySelector('.category--selected');
+    active.classList.remove('category--selected');
+    target.classList.add('category--selected');
+}
+
+//Filtering projects
+function filterProjects(filter){
     projects.forEach((project) => {
         if(filter ==='all' || filter === project.dataset.type){
-            project.computedStyleMap.display = 'block';
+            project.style.display = 'block';
         }else{
-            project.computedStyleMap.display = 'none';
+            project.style.display = 'none';
         }
     });
-});
+
+    //Set Animation effects
+    projectContainer.classList.add('anim-out');
+
+    //Off Animation effects
+    setTimeout(()=>{
+        projectContainer.classList.remove('anim-out');
+    }, 250)
+}
 
