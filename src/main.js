31894965d9a10894918a807fb530027799ab4af9
EheyGIGI <142112460+EheyGIGI@users.noreply.github.com>
2023-09-08 14:51:21 +0100

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -19,11 +19,23 @@ document.addEventListener('scroll', ()=> {
 });
 
 //visibling arrow icon when it's far away from home section
-const arrowUp = document.querySelector('.arrow-up');
+const arrowUp = document.querySelectors('.arrow-up');
 document.addEventListener('scroll', ()=>{
     if(window.scrollY > homeHeight / 2){
         arrowUp.style.opacity = 1;
     }else{
         arrowUp.style.opacity = 0;
     }
+});
+
+//Handling Toggle butten for navigation bar in the header
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', ()=>{
+    navbarMenu.classList.toggle('open');
+});
+
+// Automatically close when you click the navigation bar
+navbarMenu.addEventListener('click', ()=>{
+    navbarMenu.classList.remove('open');
 });
