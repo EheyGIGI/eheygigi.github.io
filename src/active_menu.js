3bdf8fbffa2e3f06a4e08eabe0290d1c5f9784eb
EheyGIGI <142112460+EheyGIGI@users.noreply.github.com>
2023-09-14 22:40:33 +0100

    Update active_menu.js

--- src/active_menu.js
+++ src/active_menu.js
@@ -8,13 +8,13 @@ const visibleSections = sectionIds.map(()=> false); //기본적으로 비워줌
 
 const options = {};
 const observer = new IntersectionObserver(observerCallback, options);
-sections.forEach((section) => observer.observe(section));
+sections.forEach((section) => {observer.observe(section)});
 
 function observerCallback(entries){
     entries.forEach((entry) => {
         const index = sectionIds.indexOf(`#${entry.target.id}`);
         console.log(entry.target.id);
-        console.log(index);
+        //console.log(index);
         // console.log(entry.target);
         // console.log(entry.isIntersecting);
         // console.log(entry.intersectionRatio);
