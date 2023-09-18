52b9d8930a40b19d071ae717e090b83eab5c1be3
EheyGIGI <142112460+EheyGIGI@users.noreply.github.com>
2023-09-18 21:56:11 +0100

    JS-ActiveMenu

--- src/active_menu.js
+++ src/active_menu.js
@@ -1,20 +1,35 @@
-const sectionIds = ['#home', '#about', '#skills', '#about', '#testimonial', '#contact'];
+const sectionIds = ['#home', '#about', '#skills', '#works', '#testimonial', '#contact'];
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) => document.querySelector(`[href="${id}"]`));
 
-const visibleSections = sectionIds.map(()=> false); //기본적으로 비워줌
-
-const options = {};
+const visibleSections = sectionIds.map(()=> false);
+const options = {
+    rootMargin: '-20% 0px 0px 0px',
+    threshold : [0,0.98],
+}
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => {observer.observe(section)});
 
 function observerCallback(entries){
+    let selectLastOne;
     entries.forEach((entry) => {
         const index = sectionIds.indexOf(`#${entry.target.id}`);
-        console.log(entry.target.id);
-        //console.log(index);
-        // console.log(entry.target);
-        // console.log(entry.isIntersecting);
-        // console.log(entry.intersectionRatio);
-    })
+        visibleSections[index] = entry.isIntersecting;
+        selectLastOne = 
+            index === sectionIds.length - 1 && 
+            entry.isIntersecting && 
+            entry.intersectionRatio >= 0.99;
+    });
+    console.log(visibleSections);
+    console.log('무조건 라스트 섹션!', selectLastOne);
+    
+    const navIndex = selectLastOne 
+        ? sectionIds.length - 1 
+        : findFirstIntersection(visibleSections);
+    console.log(sectionIds[navIndex]);
+}
+
+function findFirstIntersection(intersections){
+    const index = intersections.indexOf(true);
+    return index >= 0 ? index : 0;
 }
